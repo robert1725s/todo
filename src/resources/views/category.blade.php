b385b0a1d792c1d1a85d4adb812a2188906720e2
robert1725s <shunsuke.robert@gmail.com>
2025-08-01 21:22:25 +0900

    トップページ・カテゴリ一覧ページ作成

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app') @section('css')
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/category.css') }}" />
     @endsection @section('content')
-    <div class="todo__alert">
+    <div class="category__alert">
         @if (session('message'))
-            <div class="todo__alert--success">{{ session('message') }}</div>
+            <div class="category__alert--success">{{ session('message') }}</div>
             @endif @if ($errors->any())
-                <div class="todo__alert--danger">
+                <div class="category__alert--danger">
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
@@ -14,53 +14,32 @@
                 </div>
             @endif
     </div>
-    <div class="todo__content">
-        <div class="section__title">
-            <h2>新規作成</h2>
-        </div>
-        <form class="create-form" action="/todos" method="post">
+    <div class="category__content">
+        <form class="create-form" action="/categories" method="post">
             @csrf
             <div class="create-form__item">
                 <input class="create-form__item-input" type="text" name="content" value="{{ old('content') }}" />
-                <select class="create-form__item-select">
-                    <option value="">カテゴリ</option>
-                </select>
             </div>
             <div class="create-form__button">
                 <button class="create-form__button-submit" type="submit">作成</button>
             </div>
         </form>
-        <div class="section__title">
-            <h2>Todo検索</h2>
-        </div>
-        <form class="search-form">
-            <div class="search-form__item">
-                <input class="search-form__item-input" type="text" />
-                <select class="search-form__item-select">
-                    <option value="">カテゴリ</option>
-                </select>
-            </div>
-            <div class="search-form__button">
-                <button class="search-form__button-submit" type="submit">検索</button>
-            </div>
-        </form>
-        <div class="todo-table">
-            <table class="todo-table__inner">
-                <tr class="todo-table__row">
-                    <th class="todo-table__header">
-                        <span class="todo-table__header-span">Todo</span>
-                        <span class="todo-table__header-span">カテゴリ</span>
+        <div class="category-table">
+            <table class="category-table__inner">
+                <tr class="category-table__row">
+                    <th class="category-table__header">
+                        <span class="category-table__header-span">category</span>
                     </th>
                 </tr>
-                @foreach ($todos as $todo)
-                    <tr class="todo-table__row">
-                        <td class="todo-table__item">
-                            <form class="update-form" action="/todos/update" method="post">
+                @foreach ($categories as $category)
+                    <tr class="category-table__row">
+                        <td class="category-table__item">
+                            <form class="update-form" action="/categorys/update" method="post">
                                 @method('PATCH') @csrf
                                 <div class="update-form__item">
                                     <input class="update-form__item-input" type="text" name="content"
-                                        value="{{ $todo['content'] }}" />
-                                    <input type="hidden" name="id" value="{{ $todo['id'] }}" />
+                                        value="{{ $category['content'] }}" />
+                                    <input type="hidden" name="id" value="{{ $category['id'] }}" />
                                 </div>
                                 <div class="update-form__item">
                                     <p class="update-form__item-p">Category 1</p>
@@ -72,8 +51,8 @@
                                 </div>
                             </form>
                         </td>
-                        <td class="todo-table__item">
-                            <form class="delete-form" action="/todos/delete" method="post">
+                        <td class="category-table__item">
+                            <form class="delete-form" action="/categorys/delete" method="post">
                                 @method('DELETE') @csrf
                                 <div class="delete-form__button">
                                     <button class="delete-form__button-submit" type="submit">
